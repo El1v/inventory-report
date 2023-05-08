4fcb73d5037145b23435be5ae241de023aa1eefb
Elivelton <eliveltonmn@outlook.com>
2023-05-08 17:05:18 -0300

    feat: funcao complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products_list):
+
+        simple_report = SimpleReport.generate(products_list)
+        companies_data = [
+            product["nome_da_empresa"] for product in products_list
+        ]
+        companies_counter = Counter(companies_data)
+        report = ""
+        for companie, quantity in companies_counter.items():
+            report += f"- {companie}: {quantity}\n"
+
+        return (
+            f"{simple_report}"
+            f"\nProdutos estocados por empresa:\n"
+            f"{report}"
+        )
