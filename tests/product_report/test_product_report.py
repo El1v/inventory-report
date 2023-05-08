19dd96092c2bca5f514484d8ecd65e9706976efa
Elivelton <eliveltonmn@outlook.com>
2023-05-08 18:25:14 -0300

    feat: teste para o metodo repr da instancia de produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nome do Produto",
+        "Nome da Empresa",
+        "08-05-2023",
+        "10-05-2023",
+        "Numero de Série",
+        "...instrucoes...",
+    )
+
+    assert product.__repr__() == (
+        "O produto Nome do Produto fabricado em 08-05-2023 "
+        "por Nome da Empresa com validade até 10-05-2023 "
+        "precisa ser armazenado ...instrucoes...."
+    )
